2d93e84319003fcded3674f4e4487936e2482351
vamsi876 <vamsikrishnakollipara1234@gmail.com>
2025-03-30 17:11:26 -0400

    Configure for GitHub Pages deployment

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
-  base: mode === 'production' ? '/' : '/',
+  base: mode === 'production' ? '/vamsikrishna-portfolio/' : '/',
   server: {
     host: true,
     port: 8082,
